1fbfdf8127bb6a4baf9fbe941ceeb363b71b3d28
kkllPreciel <zie35-gymiko@jcom.zaq.ne.jp>
2017-03-28 02:33:29 +0900

    メインループ処理を追加

--- QtDirect3D12/QtDirect3D12/QtDirect3D12.cpp
+++ QtDirect3D12/QtDirect3D12/QtDirect3D12.cpp
@@ -1,18 +1,38 @@
 #include <qevent.h>
+#include <qtimer.h>
 #include "QtDirect3D12.h"
 #include "../Sein/Direct3D12Device.h"
 
 QtDirect3D12::QtDirect3D12(QWidget *parent)
-	: QWidget(parent)
+	: QWidget(parent),
+	timer(new QTimer)
 {
 	ui.setupUi(this);
 
-	auto pDevice = new Sein::Direct3D12::Device();
-
+	// DirectX12のラッパーを作成
 	// TODO:reinterpret_castを削除する
+	auto pDevice = new Sein::Direct3D12::Device();
 	pDevice->Create(reinterpret_cast<HWND>(this->winId()), this->width(), this->height());
-
 	delete pDevice;
+
+	// メインループ呼び出し設定
+	connect(timer, SIGNAL(timeout()), this, SLOT(mainLoop()));
+	timer->start(1000 / 60);
+}
+
+QtDirect3D12::~QtDirect3D12()
+{
+	if (timer->isActive())
+	{
+		timer->stop();
+	}
+
+	delete timer;
+}
+
+void QtDirect3D12::mainLoop()
+{
+	OutputDebugString(L"exe\n");
 }
 
 void QtDirect3D12::resizeEvent(QResizeEvent *event)
--- QtDirect3D12/QtDirect3D12/QtDirect3D12.h
+++ QtDirect3D12/QtDirect3D12/QtDirect3D12.h
@@ -9,10 +9,15 @@ class QtDirect3D12 : public QWidget
 
 public:
 	QtDirect3D12(QWidget *parent = Q_NULLPTR);
+	~QtDirect3D12();
 
 protected:
 	void resizeEvent(QResizeEvent *event) Q_DECL_FINAL;
 
+private Q_SLOTS:
+	void mainLoop();
+
 private:
 	Ui::QtDirect3D12Class ui;
+	QTimer* timer;
 };
