225b937050a1e776028669cf17c42770aa9fe881
kkllPreciel <zie35-gymiko@jcom.zaq.ne.jp>
2017-04-30 23:53:14 +0900

    Pmx読み込み用クラスを追加

---
+++ QtDirect3D12/QtDirect3D12/PMX/Loader.cpp
@@ -0,0 +1,23 @@
+/**
+ *	@file		Loader.cpp
+ *	@brief		Pmx読み込みに関する処理を行うプログラムソース
+ *	@author		kkllPreciel
+ *	@date		2017/04/30
+ *	@version	1.0
+ */
+
+#include "Loader.h"
+
+namespace Sein
+{
+	namespace Pmx
+	{
+		/**
+		 *	@brief	コンストラクタ
+		 */
+		Loader::Loader()
+		{
+
+		}
+	};
+};
---
+++ QtDirect3D12/QtDirect3D12/PMX/Loader.h
@@ -0,0 +1,29 @@
+/**
+ *	@file		Loeader.h
+ *	@brief		Pmx読み込みに関するヘッダファイル
+ *	@author		kkllPreciel
+ *	@date		2017/04/30
+ *	@version	1.0
+ */
+
+#pragma once
+
+// include
+
+namespace Sein
+{
+	namespace Pmx
+	{
+		/**
+		 *	@brief	Pmx読み込み用クラス
+		 */
+		class Loader
+		{
+		public:
+			/**
+			 *	@brief	コンストラクタ
+			 */
+			Loader();
+		};
+	};
+};
