3e00d4ab8e56721ee1873457304cc899750117e0
kkllPreciel <zie35-gymiko@jcom.zaq.ne.jp>
2017-08-16 23:59:09 +0900

    定数バッファ関連の処理をアプリケーション側へ移動

--- QtDirect3D12/QtDirect3D12/QtDirect3D12.cpp
+++ QtDirect3D12/QtDirect3D12/QtDirect3D12.cpp
@@ -4,6 +4,7 @@
 #include <Sein\Direct3D12\direct3d12_device.h>
 #include <Sein\Direct3D12\vertex_buffer.h>
 #include <Sein\Direct3D12\index_buffer.h>
+#include <Sein\Direct3D12\constant_buffer.h>
 #include <Sein\PMX\loader.h>
 #include "QtDirect3D12.h"
 
@@ -12,7 +13,8 @@ QtDirect3D12::QtDirect3D12(QWidget *parent)
   timer(new QTimer),
   device(new Sein::Direct3D12::Device()),
   vertexBuffer(new Sein::Direct3D12::VertexBuffer),
-  indexBuffer(new Sein::Direct3D12::IndexBuffer)
+  indexBuffer(new Sein::Direct3D12::IndexBuffer),
+  constantBufferView(nullptr)
 {
   ui.setupUi(this);
 
@@ -21,9 +23,11 @@ QtDirect3D12::QtDirect3D12(QWidget *parent)
   device->Create(reinterpret_cast<HWND>(this->winId()), this->width(), this->height());
   Sein::Pmx::Loader loader;
   loader.Load("../Resources/Pmx/YYB式桜ミクv1.00.pmx");
-  //loader.Load("D:/DiskD/Study/Qt/QtDirect3D12/QtDirect3D12/Resources/Pmx/Tda式初音ミク・アペンド_Ver1.10.pmx");
   vertexBuffer->Create(&(device->GetDevice()), loader.GetVertexSize() * loader.GetVertexCount(), loader.GetVertexSize(), loader.GetVertices());
   indexBuffer->Create(&(device->GetDevice()), loader.GetIndexSize() * loader.GetIndexCount(), loader.GetIndices(), DXGI_FORMAT_R32_UINT);
+
+  // 定数バッファを作成
+  constantBufferView.reset(device->CreateConstantBuffer(sizeof(ConstantBufferType)));
 
   // メインループ呼び出し設定
   connect(timer.get(), SIGNAL(timeout()), this, SLOT(mainLoop()));
@@ -40,20 +44,47 @@ QtDirect3D12::~QtDirect3D12()
 
 void QtDirect3D12::mainLoop()
 {
-  static QTime time;
-  static auto frameCount = 0;
-  wchar_t buf[256] = { 0 };
-
-  swprintf_s<256>(buf, L"FPS is %f\n", (float)(frameCount / (time.elapsed() / 1000.0f)));
-
-  if (frameCount == 0)
-    time.start();
-  else
+  // デバッグ用FPS表示
   {
-    OutputDebugString(buf);
+    static QTime time;
+    static auto frameCount = 0;
+    wchar_t buf[256] = { 0 };
+
+    swprintf_s<256>(buf, L"FPS is %f\n", (float)(frameCount / (time.elapsed() / 1000.0f)));
+
+    if (frameCount == 0)
+      time.start();
+    else
+    {
+      OutputDebugString(buf);
+    }
+
+    frameCount++;
   }
 
-  frameCount++;
+  // 定数バッファを更新
+  {
+    static float now = 0.0f;
+    static float angle = DirectX::XM_PI / 180.0f;
+
+    // 回転
+    now += angle;
+
+    // ワールド行列を更新
+    DirectX::XMStoreFloat4x4(&(constantBuffer.world), DirectX::XMMatrixRotationY(now));
+
+    // ビュー行列を作成
+    DirectX::XMVECTORF32 eye = { 0.0f, 10.0f, -30.5f, 0.0f };
+    DirectX::XMVECTORF32 at = { 0.0f, 10.0f, 0.0f, 0.0f };
+    DirectX::XMVECTORF32 up = { 0.0f, 1.0f, 0.0f, 0.0f };
+    DirectX::XMStoreFloat4x4(&(constantBuffer.view), DirectX::XMMatrixLookAtLH(eye, at, up));
+
+    // プロジェクション行列を作成
+    DirectX::XMStoreFloat4x4(&(constantBuffer.projection), DirectX::XMMatrixPerspectiveFovLH(DirectX::XM_PI / 3.0f, 600.0f / 400.0f, 0.1f, 1000.0f));
+
+    // 定数バッファを更新
+    constantBufferView->Map(&constantBuffer, sizeof(ConstantBufferType));
+  }
 
   device->BeginScene();
   device->Render(*vertexBuffer, *indexBuffer);
--- QtDirect3D12/QtDirect3D12/QtDirect3D12.h
+++ QtDirect3D12/QtDirect3D12/QtDirect3D12.h
@@ -2,35 +2,49 @@
 
 #include <memory>
 #include <QtWidgets/QWidget>
+#include <DirectXMath.h>
 #include "ui_QtDirect3D12.h"
 
 namespace Sein {
-	namespace Direct3D12 {
-		class Device;
-		class VertexBuffer;
-		class IndexBuffer;
-	};
+  namespace Direct3D12 {
+    class Device;
+    class VertexBuffer;
+    class IndexBuffer;
+    class ConstantBuffer;
+  };
 };
 
 class QtDirect3D12 : public QWidget
 {
-	Q_OBJECT
+  Q_OBJECT
 
 public:
-	QtDirect3D12(QWidget *parent = Q_NULLPTR);
-	~QtDirect3D12();
+  QtDirect3D12(QWidget *parent = Q_NULLPTR);
+  ~QtDirect3D12();
 
 protected:
-	void resizeEvent(QResizeEvent *event) Q_DECL_FINAL;
+  void resizeEvent(QResizeEvent *event) Q_DECL_FINAL;
 
 private Q_SLOTS:
-	void mainLoop();
+  void mainLoop();
 
 private:
-	Ui::QtDirect3D12Class ui;
-	std::unique_ptr<QTimer> timer;
-	std::unique_ptr<Sein::Direct3D12::Device> device;
-	std::unique_ptr<Sein::Direct3D12::VertexBuffer> vertexBuffer;
-	std::unique_ptr<Sein::Direct3D12::IndexBuffer> indexBuffer;
+  Ui::QtDirect3D12Class ui;
+  std::unique_ptr<QTimer> timer;
+  std::unique_ptr<Sein::Direct3D12::Device> device;
+  std::unique_ptr<Sein::Direct3D12::VertexBuffer> vertexBuffer;
+  std::unique_ptr<Sein::Direct3D12::IndexBuffer> indexBuffer;
+
+  /**
+   *  @brief  定数バッファ用構造体
+   */
+  struct ConstantBufferType
+  {
+    DirectX::XMFLOAT4X4 world;      ///< ワールド行列(世界空間)
+    DirectX::XMFLOAT4X4 view;       ///< ビュー行列(視線空間)
+    DirectX::XMFLOAT4X4 projection; ///< プロジェクション行列(射影空間)
+  };
+  ConstantBufferType constantBuffer;                                    ///< 定数バッファ
+  std::unique_ptr<Sein::Direct3D12::ConstantBuffer> constantBufferView; ///< 定数バッファビュー
 
 };
