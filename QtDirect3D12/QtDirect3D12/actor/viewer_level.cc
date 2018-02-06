839ae95ba4017c5b19dbc8be73c8c9a9d07750bd
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2018-02-06 23:57:00 +0900

    ビューア用レベルクラスを追加

---
+++ QtDirect3D12/QtDirect3D12/actor/viewer_level.cc
@@ -0,0 +1,96 @@
+/**
+ *  @file     viewer_level.cc
+ *  @brief    ビューア用レベルに関する処理を行うプログラムソース
+ *  @author   kkllPreciel
+ *  @date     2018/02/06
+ *  @version  1.0
+ */
+
+ // include
+#include "actor/viewer_level.h"
+#include "job_system/job_scheduler.h"
+#include "job_system/async_job_manager.h"
+
+namespace App
+{
+  namespace actor
+  {
+    /**
+     *  @brief  コンストラクタ
+     */
+    ViewerLevel::ViewerLevel()
+    {
+
+    }
+
+    /**
+     *  @brief  デストラクタ
+     */
+    ViewerLevel::~ViewerLevel()
+    {
+      Destroy();
+    }
+
+    /**
+     *  @brief  初期化を行う
+     *  @return 初期化成功フラグ
+     */
+    bool ViewerLevel::Create()
+    {
+      return true;
+    }
+
+    /**
+     *  @brief  更新処理を行う
+     *  @param  delta_time:1フレームの経過時間
+     */
+    void ViewerLevel::Execute(std::uint64_t delta_time)
+    {
+      if (initialized_ == false && async_job_.IsFinished())
+      {
+        initialized_ = true;
+      }
+    }
+
+    /**
+     *  @brief  終了処理を行う
+     */
+    void ViewerLevel::Destroy()
+    {
+
+    }
+
+    /**
+     *  @brief  モデルを読み込む
+     *  @param  file_path:読み込むモデルのファイルパス
+     */
+    void ViewerLevel::LoadModel(std::string file_path)
+    {
+      Destroy();
+
+      job_.SetFunction([&](std::uint64_t delta_time) { this->Execute(delta_time); });
+      job_system::JobScheduler::GetInstance()->Register(&job_, job_system::JobScheduler::kMoveUpdate);
+
+      // 非同期関数のジョブ登録
+      async_job_.SetFunction([&]() { this->AsyncExecute(); });
+      job_system::AsyncJobManager::GetInstance()->Push(&async_job_);
+    }
+
+    /**
+     *  @brief  読み込み終了イベントを登録する
+     *  @param  callback:読み込み終了時に実行する関数
+     */
+    void ViewerLevel::RegisterLoadedEvent(std::function<void()> callback)
+    {
+      function_ = callback;
+    }
+
+    /**
+     *  @brief  非同期実行処理関数
+     */
+    void ViewerLevel::AsyncExecute()
+    {
+      // 非同期読み込み処理
+    }
+  };
+};
---
+++ QtDirect3D12/QtDirect3D12/actor/viewer_level.h
@@ -0,0 +1,96 @@
+/**
+ *  @file     viewer_level.h
+ *  @brief    ビューア用レベルに関するヘッダファイル
+ *  @author   kkllPreciel
+ *  @date     2018/02/06
+ *  @version  1.0
+ */
+
+#pragma once
+
+ // include
+#include <cstdint>
+#include <d3d12.h>
+#include <Sein\Direct3D12\vertex_buffer.h>
+#include <Sein\Direct3D12\index_buffer.h>
+#include "job_system/job.h"
+#include "job_system/async_job.h"
+#include "actor/actor.h"
+
+namespace App
+{
+  namespace actor
+  {
+    /**
+     *  @brief  ビューア用レベルクラス
+     */
+    class ViewerLevel final
+    {
+    public:
+      /**
+       *  @brief  コンストラクタ
+       */
+      ViewerLevel();
+
+      /**
+       *  @brief  デストラクタ
+       */
+      ~ViewerLevel();
+
+      /**
+       *  @brief  コピーコンストラクタ
+       *  @param  other:コピー元のインスタンス
+       */
+      ViewerLevel(const ViewerLevel& other) = delete;
+      
+      /**
+       *  @brief  コピー代入演算子オペレータ
+       *  @param  other:コピー代入元のインスタンス
+       *  @return コピー代入後のインスタンス
+       */
+      ViewerLevel& operator = (const ViewerLevel& other) = delete;
+
+      /**
+       *  @brief  初期化を行う
+       *  @return 初期化成功フラグ
+       */
+      bool Create();
+
+      /**
+       *  @brief  更新処理を行う
+       *  @param  delta_time:1フレームの経過時間
+       */
+      void Execute(std::uint64_t delta_time);
+
+      /**
+       *  @brief  終了処理を行う
+       */
+      void Destroy();
+
+      /**
+       *  @brief  モデルを読み込む
+       *  @param  file_path:読み込むモデルのファイルパス
+       */
+      void LoadModel(std::string file_path);
+
+      /**
+       *  @brief  読み込み終了イベントを登録する
+       *  @param  callback:読み込み終了時に実行する関数
+       */
+      void RegisterLoadedEvent(std::function<void()> callback);
+
+    private:
+      /**
+       *  @brief  非同期実行処理関数
+       */
+      void AsyncExecute();
+
+    private:
+      job_system::Job job_;                           ///< 更新処理用ジョブ
+      job_system::AsyncJob async_job_;                ///< 非同期処理用ジョブ
+      bool initialized_ = false;                      ///< 初期化終了フラグ
+      std::string file_path_;                         ///< モデルファイルパス
+      std::function<void()> function_;                ///< 関数へのポインタ
+    };
+  };
+};
