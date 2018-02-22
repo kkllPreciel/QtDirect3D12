bb2d7f73b60eb41170639371721233f8cd6250af
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2018-02-22 23:55:19 +0900

    マウス操作可能なカメラ移動用コンポーネントクラスを作成

---
+++ QtDirect3D12/QtDirect3D12/actor/mouse_driven_camera_move_component.cc
@@ -0,0 +1,214 @@
+/**
+ *  @file     mouse_driven_camera_move_component.cc
+ *  @brief    マウスで操作できるカメラ移動用コンポーネントに関する処理を行うプログラムソース
+ *  @author   kkllPreciel
+ *  @date     2018/02/22
+ *  @version  1.0
+ */
+
+ // include
+#include "actor/mouse_driven_camera_move_component.h"
+#include "actor/camera_component.h"
+#include "job_system/job_scheduler.h"
+
+namespace App
+{
+  namespace actor
+  {
+    /**
+     *  @brief  引数付きコンストラクタ
+     *  @param  owner:コンポーネントを所持しているオーナー
+     */
+    MouseDrivenCameraMoveComponent::MouseDrivenCameraMoveComponent(Actor* owner) : Component(owner)
+    {
+
+    }
+
+    /**
+     *  @brief  デストラクタ
+     */
+    MouseDrivenCameraMoveComponent::~MouseDrivenCameraMoveComponent()
+    {
+      Destroy();
+    }
+
+    /**
+     *  @brief  初期化を行う
+     *  @return 初期化成功フラグ
+     */
+    bool MouseDrivenCameraMoveComponent::Create()
+    {
+      clicked_ = false;
+
+      Destroy();
+
+      return true;
+    }
+
+    /**
+     *  @brief  終了処理を行う
+     */
+    void MouseDrivenCameraMoveComponent::Destroy()
+    {
+      // ジョブスケジューラへの登録を解除する
+      if (job_)
+      {
+        job_system::JobScheduler::GetInstance()->Unregister(job_.get());
+      }
+
+      job_.reset();
+
+      delta_ = 0;
+    }
+
+    /**
+     *  @brief  更新処理を行う
+     *  @param  delta_time:1フレームの経過時間
+     */
+    void MouseDrivenCameraMoveComponent::Update(std::uint64_t delta_time)
+    {
+      delta_ += delta_time;
+      auto time = static_cast<float>(delta_) / static_cast<float>(time_ * 60);
+
+      DirectX::XMVECTOR now = DirectX::XMVectorLerp(begin_position_, end_position_, time);
+      //now = DirectX::XMVectorAdd(DirectX::XMVectorScale(begin_position_, (1.0f - time_)), DirectX::XMVectorScale(end_position_, time_));
+      owner_->SetPosition(now);
+
+      // 移動が終了したら自動的に削除する
+      if (time_ * 60 <= delta_)
+      {
+        Destroy();
+      }
+    }
+
+    /**
+     *  @brief  移動速度を設定する
+     *  @param  velocity:移動速度
+     */
+    void MouseDrivenCameraMoveComponent::SetVelocity(const float velocity)
+    {
+      velocity_ = velocity;
+    }
+    
+    /**
+     *  @brief  移動係数を設定する
+     *  @param  coefficient:移動係数
+     */
+    void MouseDrivenCameraMoveComponent::SetCoefficient(const float coefficient)
+    {
+      coefficient_ = coefficient;
+    }
+
+    /**
+     *  @brief  移動に必要な時間を設定する
+     *  @param  time:移動に必要な時間
+     */
+    void MouseDrivenCameraMoveComponent::SetTime(std::uint64_t time)
+    {
+      time_ = time;
+    }
+
+    /**
+     *  @brief  注視点との最低距離を設定する
+     *  @param  distance:注視点との最低距離
+     */
+    void MouseDrivenCameraMoveComponent::SetDistance(const float distance)
+    {
+      assert(1.0f <= distance);
+      distance_ = distance;
+    }
+    
+    /**
+     *  @brief  マウスクリックイベントが発生した
+     *  @param  position:マウスの座標
+     */
+    void MouseDrivenCameraMoveComponent::OnMousePressEvent(DirectX::XMVECTOR position)
+    {
+      Destroy();
+
+      clicked_position_ = position;
+      clicked_ = true;
+    }
+    
+    /**
+     *  @brief  マウスリリースイベントが発生した
+     *  @param  position:マウスの座標
+     */
+    void MouseDrivenCameraMoveComponent::OnMouseReleaseEvent(DirectX::XMVECTOR position)
+    {
+      clicked_ = false;
+    }
+    
+    /**
+     *  @brief  マウス移動イベントが発生した
+     *  @param  position:マウスの座標
+     */
+    void MouseDrivenCameraMoveComponent::OnMouseMoveEvent(DirectX::XMVECTOR position)
+    {
+      if (clicked_ == false)
+      {
+        return;
+      }
+
+      // カメラから注視点へのベクトルは分かる
+      // カメラの上方向ベクトルも分かる
+      // カメラの横方向ベクトルも計算可能
+      // マウスの移動ベクトルに応じて上方向ベクトルと横方向ベクトルを合算する
+
+      auto component = owner_->GetComponent<CameraComponent>();
+      DirectX::XMVECTOR at = component->GetLookAt();
+
+      DirectX::XMVector2Normalize(DirectX::XMVectorSubtract(position, clicked_position_));
+
+      // カメラの回転を行う
+    }
+    
+    /**
+     *  @brief  マウスホイールイベントが発生した
+     *  @param  direction:ホイールの回転方向
+     */
+    void MouseDrivenCameraMoveComponent::OnMouseWheelEvent(std::int32_t direction)
+    {
+      Destroy();
+
+      if (clicked_)
+      {
+        return;
+      }
+
+      // 視点から注視点へのベクトルを作成
+      DirectX::XMVECTOR eye = owner_->GetPosition();
+      DirectX::XMVECTOR at = owner_->GetComponent<CameraComponent>()->GetLookAt();
+      DirectX::XMVECTOR dir = DirectX::XMVectorSubtract(at, eye);
+
+      // 視点から注視点への距離を取得
+      float distance = 0.0f;
+      DirectX::XMStoreFloat(&distance, DirectX::XMVector3Length(dir));
+
+      // 距離が離れている場合は移動量に補正を行う
+      const auto move = velocity_ * (std::fabsf(distance) * coefficient_);
+
+      DirectX::XMVECTOR target = DirectX::XMVectorAdd(eye, DirectX::XMVectorScale(DirectX::XMVector3Normalize(dir), direction * move));
+
+      // 注視点との距離が一定以下なら近づけないようにする
+      dir = DirectX::XMVectorSubtract(at, target);
+      distance = 0.0f;
+      DirectX::XMStoreFloat(&distance, DirectX::XMVector3Length(dir));
+      if (distance < distance_)
+      {
+        return;
+      }
+
+      begin_position_ = eye;
+      end_position_ = target;
+
+      // ジョブに登録する
+      job_ = std::make_unique<job_system::Job>();
+
+      job_->SetFunction([&](std::uint64_t delta_time) {
+        this->Update(delta_time);
+      });
+      job_system::JobScheduler::GetInstance()->Register(job_.get(), job_system::JobScheduler::kMoveUpdate);
+    }
+  };
+};
---
+++ QtDirect3D12/QtDirect3D12/actor/mouse_driven_camera_move_component.h
@@ -0,0 +1,143 @@
+/**
+ *  @file     mouse_driven_camera_move_component.h
+ *  @brief    マウスで操作できるカメラ移動用コンポーネントに関するヘッダファイル
+ *  @author   kkllPreciel
+ *  @date     2018/02/22
+ *  @version  1.0
+ */
+
+#pragma once
+
+ // include
+#include <memory>
+#include <DirectXMath.h>
+#include "actor/component.h"
+#include "job_system/job.h"
+
+namespace App
+{
+  namespace actor
+  {
+    /**
+     *  @brief  マウスで操作できるカメラ移動用のコンポーネントクラス
+     */
+    class MouseDrivenCameraMoveComponent : public Component
+    {
+    public:
+      /**
+       *  @brief  引数付きコンストラクタ
+       *  @param  owner:コンポーネントを所持しているオーナー
+       */
+      MouseDrivenCameraMoveComponent(Actor* owner);
+
+      /**
+       *  @brief  デストラクタ
+       */
+      virtual ~MouseDrivenCameraMoveComponent();
+
+      /**
+       *  @brief  コンストラクタ
+       */
+      MouseDrivenCameraMoveComponent() = delete;
+
+      /**
+       *  @brief  コピーコンストラクタ
+       *  @param  other:コピー元のインスタンス
+       */
+      MouseDrivenCameraMoveComponent(const MouseDrivenCameraMoveComponent& other) = delete;
+      
+      /**
+       *  @brief  コピー代入演算子オペレータ
+       *  @param  other:コピー代入元のインスタンス
+       *  @return コピー代入後のインスタンス
+       */
+      MouseDrivenCameraMoveComponent& operator = (const MouseDrivenCameraMoveComponent& other) = delete;
+
+      /**
+       *  @brief  コンポーネントの種別IDを取得する
+       *  @return コンポーネントの種別ID
+       */
+      static constexpr std::uint32_t GetId()
+      {
+        return Component::ID::kMove;
+      }
+
+      /**
+       *  @brief  初期化を行う
+       *  @return 初期化成功フラグ
+       */
+      virtual bool Create() override;
+
+      /**
+       *  @brief  終了処理を行う
+       */
+      virtual void Destroy() override;
+
+      /**
+       *  @brief  更新処理を行う
+       *  @param  delta_time:1フレームの経過時間
+       */
+      void Update(std::uint64_t delta_time);
+
+      /**
+       *  @brief  移動速度を設定する
+       *  @param  velocity:移動速度
+       */
+      void SetVelocity(const float velocity);
+
+      /**
+       *  @brief  移動係数を設定する
+       *  @param  coefficient:移動係数
+       */
+      void SetCoefficient(const float coefficient);
+
+      /**
+       *  @brief  注視点との最低距離を設定する
+       *  @param  distance:注視点との最低距離
+       */
+      void SetDistance(const float distance);
+
+      /**
+       *  @brief  移動に必要な時間を設定する
+       *  @param  time:移動に必要な時間
+       */
+      void SetTime(std::uint64_t time);
+
+      /**
+       *  @brief  マウスクリックイベントが発生した
+       *  @param  position:マウスの座標
+       */
+      void OnMousePressEvent(DirectX::XMVECTOR position);
+
+      /**
+       *  @brief  マウスリリースイベントが発生した
+       *  @param  position:マウスの座標
+       */
+      void OnMouseReleaseEvent(DirectX::XMVECTOR position);
+
+      /**
+       *  @brief  マウス移動イベントが発生した
+       *  @param  position:マウスの座標
+       */
+      void OnMouseMoveEvent(DirectX::XMVECTOR position);
+
+      /**
+       *  @brief  マウスホイールイベントが発生した
+       *  @param  direction:ホイールの回転方向
+       */
+      void OnMouseWheelEvent(std::int32_t direction);
+
+    private:
+      std::unique_ptr<job_system::Job> job_;  ///< ジョブ
+      float velocity_;                        ///< 移動速度
+      float coefficient_;                     ///< 移動係数
+      float distance_;                        ///< 注視点との最低距離
+      DirectX::XMVECTOR begin_position_;      ///< 初期座標
+      DirectX::XMVECTOR end_position_;        ///< 目標座標
+      std::uint64_t time_;                    ///< 移動に必要な時間
+      std::uint64_t delta_;                   ///< 現在の経過時間
+      DirectX::XMVECTOR clicked_position_;    ///< クリック時点での座標
+      bool clicked_;                          ///< クリックフラグ
+    };
+  };
+};
